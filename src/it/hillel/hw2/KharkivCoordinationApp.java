579ced28209a09b61c9e4809d78da99edc92b7fb
oleksiiTilinin <110196861+oleksiiTilinin@users.noreply.github.com>
2022-10-04 09:07:15 +0300

    Add new app with displaying coordination of the Kharkiv city.

---
+++ src/it/hillel/hw2/KharkivCoordinationApp.java
@@ -0,0 +1,11 @@
+package it.hillel.hw2;
+
+public class KharkivCoordinationApp {
+    public static void main(String[] args) {
+        double latitude = 49.9808100;
+        double longitude = 36.2527200;
+
+        System.out.println("The coordination of my lovely city Kharkiv is - latitude: "
+                + latitude + ", longitude: " + longitude + ".");
+    }
+}
