6395fcb675aa7a002f7ff27e395fa12cabb189b4
oleksiiTilinin <110196861+oleksiiTilinin@users.noreply.github.com>
2022-10-07 09:48:03 +0300

    Add the new class for calculated amount of parallelepiped

---
+++ out/src/ithillel/hw3/ParallelepipedVolume.java
@@ -0,0 +1,12 @@
+package ithillel.hw3;
+
+public class ParallelepipedVolume {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        int volume = a * b * c;
+
+        System.out.println("Объем параллелепипеда = " + volume);
+    }
+}
