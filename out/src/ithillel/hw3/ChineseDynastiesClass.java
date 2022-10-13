276c1f14ecc2e97d7a2bdbe4e20371e8b2c361b4
oleksiiTilinin <110196861+oleksiiTilinin@users.noreply.github.com>
2022-10-13 19:25:02 +0300

    Change the logic of method

--- out/src/ithillel/hw3/ChineseDynastiesClass.java
+++ out/src/ithillel/hw3/ChineseDynastiesClass.java
@@ -3,31 +3,11 @@ package ithillel.hw3;
 public class ChineseDynastiesClass {
 
     public static void main(String[] args) {
-//        final double kof = 1.5;
-//
-//        int liWarrior = 13 * 860;
-//        int liArcher = 24 * 860;
-//        int liRider = 46 * 860;
-//        int liQuantityOfWarriors = 860 * 3;
-//
-//        int mingWarrior = (int) (9 * 860 * kof);
-//        int mingArcher = (int) (35 * 860 * kof);
-//        int mingRider = (int) (12 * 860 * kof);
-//        int mingQuantityOfWarriors = (int) (liQuantityOfWarriors * 1.5);
-//
-//        int liAttackAmount = (liWarrior + liArcher + liRider) / liQuantityOfWarriors;
-//        int mingAttack = (mingWarrior + mingArcher + mingRider) / mingQuantityOfWarriors;
-//
-//        System.out.println("Common attack value for Ling dynasty is: - " + liAttackAmount);
-//        System.out.println("Common attack value for Ming dynasty is: - " + mingAttack);
-//
 
-
-        int li = (13+24+46)*860;
-        int ming = (int) ((9+35+12)*(860*1.5));
+        int li = (13 + 24 + 46) * 860;
+        double ming =  (9 + 35 + 12) * (860 * 1.5);
         System.out.println("Li army power " + li);
         System.out.println("Ming army power " + ming);
-
 
 
     }
