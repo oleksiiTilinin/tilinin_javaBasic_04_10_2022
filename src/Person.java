8808f3d034cdc0b870b6191019d4c01679cac953
oleksiiTilinin <110196861+oleksiiTilinin@users.noreply.github.com>
2022-11-03 17:45:39 +0200

    Add new calss to work with the methods/function.

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    public String personInfo(String name, String surname, String city, int phoneNumber) {
+        return "Call to the " + name + " " + surname + " from the city " + city + " you can by this phone number: " + "'" + phoneNumber + "'";
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println(person.personInfo("Alex", "Tilinin", "Kremenchug", 9379992));
+        System.out.println(person.personInfo("Vasyl", "Pupkin", "Kharkiv", 2314678));
+        System.out.println(person.personInfo("Jora", "Golovkin", "Kyiv", 123465));
+
+    }
+}
