ff09ba0abbd1f7b9df4b2bb6d4c01b4425d48162
oleksiiTilinin <110196861+oleksiiTilinin@users.noreply.github.com>
2022-11-08 10:59:37 +0200

    Added new classes for work with constructors.

--- src/BurgerMain.java
+++ src/BurgerMain.java
@@ -1,7 +1,7 @@
 public class BurgerMain {
 
     public static void main(String[] args) {
-        Burger ordinaryBurger = new Burger("bun", "pork", "gaude", "mayonnaise");
+        Burger ordinaryBurger = new Burger("bun", "pork", "gouda", "mayonnaise");
         Burger dietBurger = new Burger("wheat bun", "chicken", "feta cheese");
         Burger doubleMeatBurger = new Burger("bun", "pork", "beef", "cheddar", "mayonnaise");
     }
